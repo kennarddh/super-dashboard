b8ac4de85a5ea581d94029a0c19d224eeab44614
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-05-20 22:11:11 +0700

    feat: React grid layout basic example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,70 @@
 import React from 'react'
 
+import RGL, { WidthProvider } from 'react-grid-layout'
+import 'react-grid-layout/css/styles.css'
+import 'react-resizable/css/styles.css'
+
+const ReactGridLayout = WidthProvider(RGL)
+
 const App = () => {
-	return <></>
+	const items = [
+		'1',
+		'2',
+		'3',
+		'4',
+		'5',
+		'6',
+		'7',
+		'8',
+		'9',
+		'10',
+		'11',
+		'12',
+		'13',
+		'14',
+		'15',
+		'16',
+		'17',
+		'18',
+		'19',
+		'20',
+	]
+
+	const layout = items.map(i => {
+		const y = Math.ceil(Math.random() * 4) + 1
+
+		return {
+			x: (i * 2) % 12,
+			y: Math.floor(i / 6) * y,
+			w: 2,
+			h: y,
+			i: i.toString(),
+		}
+	})
+
+	const OnLayoutChange = layout => {
+		console.log('layout', layout)
+	}
+
+	return (
+		<ReactGridLayout
+			layout={layout}
+			onLayoutChange={OnLayoutChange}
+			{...{
+				className: 'layout',
+				items: 20,
+				rowHeight: 30,
+				cols: 12,
+				resizeHandles: ['s', 'w', 'e', 'n', 'sw', 'nw', 'se', 'ne'],
+			}}
+		>
+			{items.map(i => (
+				<div key={i} style={{ border: '1px solid black' }}>
+					<span className='text'>{i}</span>
+				</div>
+			))}
+		</ReactGridLayout>
+	)
 }
 
 export default App
