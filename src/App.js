74fb8ca19603e2678c85999f6c14c479ada509de
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-05-23 17:48:12 +0700

    feat: Video player

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import 'react-resizable/css/styles.css'
 
 // Components
 import Whiteboard from 'Components/Whiteboard/Whiteboard'
+import VideoPlayer from 'Components/VideoPlayer/VideoPlayer'
 
 import { SectionWrapper } from './Styles'
 
@@ -31,7 +32,9 @@ const App = () => {
 			</SectionWrapper>
 			<SectionWrapper key='images'>Images</SectionWrapper>
 			<SectionWrapper key='weather'>Weather</SectionWrapper>
-			<SectionWrapper key='videoPlayer'>Video Player</SectionWrapper>
+			<SectionWrapper key='videoPlayer'>
+				<VideoPlayer />
+			</SectionWrapper>
 		</ReactGridLayout>
 	)
 }
---
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactPlayer from 'react-player/youtube'
+
+const VideoPlayer = () => {
+	return (
+		<ReactPlayer
+			width='100%'
+			height='100%'
+			url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+		/>
+	)
+}
+
+export default VideoPlayer
