883920a1251e994404958c3ff2bda91444297314
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-05-23 17:56:33 +0700

    feat: Video player url input

--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import ReactPlayer from 'react-player/youtube'
 
 const VideoPlayer = () => {
+	const [Url, SetUrl] = useState(
+		'https://www.youtube.com/watch?v=ysz5S6PUM-U'
+	)
+
+	const [PlayingUrl, SetPlayingUrl] = useState(
+		'https://www.youtube.com/watch?v=ysz5S6PUM-U'
+	)
+
+	const OnUrlChange = useCallback(event => {
+		SetUrl(event.target.value)
+	}, [])
+
+	const OnPlay = useCallback(() => {
+		ReactPlayer.canPlay(Url)
+
+		SetPlayingUrl(Url)
+	}, [Url])
+
 	return (
-		<ReactPlayer
-			width='100%'
-			height='100%'
-			url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
-		/>
+		<>
+			<input
+				type='text'
+				placeholder='Youtube url'
+				onChange={OnUrlChange}
+			/>
+			<button onClick={OnPlay}>Play</button>
+			<ReactPlayer width='100%' height='90%' url={PlayingUrl} />
+		</>
 	)
 }
 
