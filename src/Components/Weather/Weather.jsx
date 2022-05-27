9df376a63e23f8a77b258dd1eee6d8b92087ba8d
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-05-27 19:42:51 +0700

    fix: Weather memory leak

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -13,8 +13,12 @@ const Weather = () => {
 	const [Search, SetSearch] = useState('')
 
 	useEffect(() => {
+		const controller = new AbortController()
+		const signal = controller.signal
+
 		fetch(
-			`https://api.openweathermap.org/data/2.5/weather?lat=${Latitude}&lon=${Longitude}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`
+			`https://api.openweathermap.org/data/2.5/weather?lat=${Latitude}&lon=${Longitude}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`,
+			{ signal }
 		)
 			.then(response => response.json())
 			.then(data => {
@@ -23,6 +27,8 @@ const Weather = () => {
 			.catch(error => {
 				console.log(error)
 			})
+
+		return () => controller.abort()
 	}, [Latitude, Longitude])
 
 	const ChangeLocation = () => {
