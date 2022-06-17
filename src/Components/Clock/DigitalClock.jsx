c4b39c6be478d85f64a156631602b92467bde18d
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-17 13:47:50 +0700

    refactor: Move state to clock component

--- src/Components/Clock/DigitalClock.jsx
+++ src/Components/Clock/DigitalClock.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import {
 	TimeContainer,
@@ -6,74 +6,14 @@ import {
 	TimeContainerParent,
 } from './DigitalClockStyles.jsx'
 
-const DigitalClock = () => {
-	const [Timezone] = useState([
-		'UTC+2',
-		'UTC+3',
-		'UTC+4',
-		'UTC+5',
-		'UTC+6',
-		'UTC+7',
-	])
-
-	const [Time, SetTime] = useState({})
-
-	useEffect(() => {
-		const intervalIds = []
-
-		if (!Timezone) return
-		if (Timezone === null) return
-		if (Timezone === undefined) return
-		if (Timezone.length < 1) return
-
-		SetTime({})
-
-		Timezone.forEach(timezone => {
-			const hourAdd = parseInt(timezone.split('UTC')[1])
-
-			const offset = new Date().getTimezoneOffset()
-
-			const date = new Date(
-				new Date().getTime() + offset * 60000 + hourAdd * 60 * 60000
-			)
-
-			const intervalId = setInterval(() => {
-				SetTime(time => {
-					let newDate = date
-
-					if (time[timezone]?.date) {
-						newDate = new Date(
-							time[timezone].date.getTime() + 1 * 1000 + 1
-						)
-					}
-
-					return {
-						...time,
-						[timezone]: {
-							hour: newDate.getHours(),
-							minute: newDate.getMinutes(),
-							second: newDate.getSeconds(),
-							date: newDate,
-						},
-					}
-				})
-			}, 1000)
-
-			intervalIds.push(intervalId)
-		})
-
-		return () => {
-			intervalIds.forEach(clearInterval)
-		}
-	}, [Timezone])
-
+const DigitalClock = ({ time }) => {
 	return (
 		<TimeContainerParent>
-			{Object.keys(Time).map(timezone => (
+			{Object.keys(time).map(timezone => (
 				<TimeContainer key={timezone}>
-					<TimeText>{Time[timezone]?.hour}</TimeText>
-					<TimeText>{Time[timezone]?.minute}</TimeText>
-					<TimeText>{Time[timezone]?.second}</TimeText>
+					<TimeText>{time[timezone]?.hour}</TimeText>
+					<TimeText>{time[timezone]?.minute}</TimeText>
+					<TimeText>{time[timezone]?.second}</TimeText>
 				</TimeContainer>
 			))}
 		</TimeContainerParent>
