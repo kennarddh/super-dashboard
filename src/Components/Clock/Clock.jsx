8ddc726af0aa57f2be7458e93091aba4e2ea69ea
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-08 17:41:50 +0700

    feat: Clock add modal

--- src/Components/Clock/Clock.jsx
+++ src/Components/Clock/Clock.jsx
@@ -1,19 +1,44 @@
 import React, { useState } from 'react'
 
+import ReactPortal from 'Components/ReactPortal/ReactPortal'
+
 // Clock
 import DigitalClock from './Digital/Digital'
 // import AnalogClock from './Analog/Analog'
 
-import { Container, AddButton } from './Styles'
+import {
+	Container,
+	AddButton,
+	ModalContainer,
+	ModalContentContainer,
+	TimezoneInput,
+	SubmitButton,
+} from './Styles'
 
 const Clock = () => {
 	const [OffsetsInSecond] = useState([0, -25200, 25200])
+
+	const [IsOpen, SetIsOpen] = useState(false)
+
+	const OnAdd = () => {
+		SetIsOpen(true)
+	}
 
 	// return <AnalogClock offsets={OffsetsInSecond} />
 	return (
 		<Container>
 			<DigitalClock offsets={OffsetsInSecond} />
-			<AddButton>Add</AddButton>
+			<AddButton onClick={OnAdd}>Add</AddButton>
+			<ReactPortal>
+				{IsOpen && (
+					<ModalContainer>
+						<ModalContentContainer>
+							<TimezoneInput placeholder='Timezone (/(GMT|UTC)[+-][0-2][0-3]:[0-5][0-9]/gi)' />
+							<SubmitButton>Add</SubmitButton>
+						</ModalContentContainer>
+					</ModalContainer>
+				)}
+			</ReactPortal>
 		</Container>
 	)
 }
--- src/Components/Clock/Styles.jsx
+++ src/Components/Clock/Styles.jsx
@@ -20,3 +20,64 @@ export const AddButton = styled.button`
 	border: none;
 	color: white;
 `
+
+export const ModalContainer = styled.div`
+	position: absolute;
+	inset: 0;
+	background-color: #b4b4b4a4;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
+
+export const ModalContentContainer = styled.form`
+	height: 20%;
+	width: 50%;
+	background-color: #ffffff;
+	border-radius: 15px;
+	padding: 20px;
+	box-sizing: border-box;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-evenly;
+	align-items: center;
+`
+
+export const TimezoneInput = styled.input`
+	width: 80%;
+	height: 40%;
+	border-radius: 15px;
+	padding: 10px 20px;
+
+	border: none;
+
+	background-color: #b6b6b6;
+
+	&::placeholder {
+		color: white;
+	}
+
+	&:focus {
+		outline: 1px solid black;
+	}
+`
+
+export const SubmitButton = styled.button`
+	width: 10%;
+	height: 40%;
+	border-radius: 15px;
+	padding: 10px 20px;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	border: none;
+
+	background-color: #b6b6b6;
+	color: white;
+
+	&:focus {
+		outline: 1px solid black;
+	}
+`
