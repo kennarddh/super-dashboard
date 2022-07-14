219db43759f77cfdd74be02472f24a2efcb0d3b6
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-14 19:42:11 +0700

    fix: Hour regex

--- src/Utils/IsValidTimezoneText.js
+++ src/Utils/IsValidTimezoneText.js
@@ -1,5 +1,5 @@
 const IsValidTimezoneText = timezoneText => {
-	const regex = /^(GMT|UTC)[+-][0-2][0-3]:[0-5][0-9]$/gi
+	const regex = /^(GMT|UTC)[+-]([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/gi
 
 	return regex.test(timezoneText)
 }
