fe77097a8d79685ba1d495b601ef11180a447f81
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-18 09:01:35 +0700

    feat: Add modal

--- src/Components/Users/Styles.jsx
+++ src/Components/Users/Styles.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const ListContainer = styled.div`
 	overflow-y: scroll;
-	height: 100%;
+	height: 90%;
 `
 
 export const ListItem = styled.div`
@@ -12,4 +12,96 @@ export const ListItem = styled.div`
 	justify-content: center;
 	align-items: center;
 	border: 1px solid black;
+`
+
+export const AddButton = styled.button`
+	background-color: #dbdbdb;
+	color: white;
+
+	border: none;
+	padding: 5px 10px;
+
+	border-radius: 15px;
+`
+
+export const Container = styled.div`
+	width: 100%;
+	height: 100%;
+`
+
+export const Header = styled.div`
+	height: 10%;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+
+	flex-direction: row;
+`
+
+export const ModalContainer = styled.div`
+	position: absolute;
+	inset: 0;
+	background-color: #b4b4b4a4;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
+
+export const ModalContentContainer = styled.form`
+	height: 60%;
+	width: 50%;
+	background-color: #ffffff;
+	border-radius: 15px;
+	padding: 20px;
+	box-sizing: border-box;
+	display: flex;
+	flex-direction: column;
+	gap: 20px;
+	justify-content: space-evenly;
+	align-items: center;
+`
+
+export const SubmitButton = styled.button`
+	width: 10%;
+	height: 40%;
+	border-radius: 15px;
+	padding: 10px 20px;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	border: none;
+
+	background-color: #b6b6b6;
+	color: white;
+
+	&:focus {
+		outline: 1px solid black;
+	}
+`
+
+export const CloseButton = styled(SubmitButton)`
+	background-color: #ff0000;
+`
+
+export const Input = styled.input`
+	width: 70%;
+	height: 40%;
+	border-radius: 15px;
+	padding: 10px 20px;
+
+	border: none;
+
+	background-color: #b6b6b6;
+
+	resize: none;
+
+	&::placeholder {
+		color: white;
+	}
+
+	&:focus {
+		outline: 1px solid black;
+	}
 `
--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,6 +1,19 @@
 import React, { useState } from 'react'
 
-import { ListContainer, ListItem } from './Styles'
+import ReactPortal from 'Components/ReactPortal/ReactPortal'
+
+import {
+	ListContainer,
+	ListItem,
+	AddButton,
+	Header,
+	Container,
+	ModalContainer,
+	ModalContentContainer,
+	SubmitButton,
+	CloseButton,
+	Input,
+} from './Styles'
 
 const Users = () => {
 	const [UsersList] = useState([
@@ -8,14 +21,74 @@ const Users = () => {
 		{ id: '8fdac97d-4a7f-452f-9e74-77bd652ed731', name: 'Bar' },
 		{ id: '7e5da25b-bcde-41bc-91f8-932606080675', name: 'Foo Bar' },
 	])
+
+	const [IsAddUserModalOpen, SetIsAddUserModalOpen] = useState(false)
+
+	const [NameValue, SetNameValue] = useState('')
+	const [PhoneValue, SetPhoneValue] = useState('')
+	const [AddressValue, SetAddressValue] = useState('')
+
+	const ShowAddUserModal = () => {
+		SetIsAddUserModalOpen(true)
+	}
+
+	const HideAddUserModal = () => {
+		SetIsAddUserModalOpen(false)
+	}
+
 	return (
-		<ListContainer>
-			{UsersList.map(user => (
-				<ListItem key={user.id}>
-					<p>{user.name}</p>
-				</ListItem>
-			))}
-		</ListContainer>
+		<Container>
+			<Header>
+				<AddButton onClick={ShowAddUserModal}>Add</AddButton>
+			</Header>
+			<ListContainer>
+				{UsersList.map(user => (
+					<ListItem key={user.id}>
+						<p>{user.name}</p>
+					</ListItem>
+				))}
+			</ListContainer>
+			<ReactPortal wrapperId='remove-clock-timezone'>
+				{IsAddUserModalOpen && (
+					<ModalContainer>
+						<ModalContentContainer>
+							<h3>Add user</h3>
+							<Input
+								value={NameValue}
+								onChange={event =>
+									SetNameValue(event.target.value)
+								}
+								placeholder='Name'
+							/>
+							<Input
+								value={PhoneValue}
+								onChange={event =>
+									SetPhoneValue(event.target.value)
+								}
+								placeholder='Phone'
+							/>
+							<Input
+								value={AddressValue}
+								onChange={event =>
+									SetAddressValue(event.target.value)
+								}
+								as='textarea'
+								rows={3}
+								placeholder='Address'
+								style={{ height: '70%' }}
+							></Input>
+							<SubmitButton type='submit'>Save</SubmitButton>
+							<CloseButton
+								type='button'
+								onClick={HideAddUserModal}
+							>
+								Close
+							</CloseButton>
+						</ModalContentContainer>
+					</ModalContainer>
+				)}
+			</ReactPortal>
+		</Container>
 	)
 }
 
