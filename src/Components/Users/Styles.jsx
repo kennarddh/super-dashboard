551fc96bbbe55f1cc7a105a50ce1edf6e70fcfb2
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-18 07:43:23 +0700

    feat: Users list

---
+++ src/Components/Users/Styles.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const ListContainer = styled.div`
+	overflow-y: scroll;
+	height: 100%;
+`
+
+export const ListItem = styled.div`
+	width: 100%;
+	height: 50px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border: 1px solid black;
+`
--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { ListContainer, ListItem } from './Styles'
 
 const Users = () => {
-	return <div></div>
+	const [UsersList] = useState([
+		{ id: '1896d3ba-715a-47a0-a4e9-c746b69589a2', name: 'Foo' },
+		{ id: '8fdac97d-4a7f-452f-9e74-77bd652ed731', name: 'Bar' },
+		{ id: '7e5da25b-bcde-41bc-91f8-932606080675', name: 'Foo Bar' },
+	])
+	return (
+		<ListContainer>
+			{UsersList.map(user => (
+				<ListItem key={user.id}>
+					<p>{user.name}</p>
+				</ListItem>
+			))}
+		</ListContainer>
+	)
 }
 
 export default Users
