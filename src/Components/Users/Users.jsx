668158f2652add0a60d21c84411d5104a0e22ac1
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-18 14:13:58 +0700

    feat: Edit user

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -19,9 +19,24 @@ import {
 
 const Users = () => {
 	const [UsersList, SetUsersList] = useState([
-		{ id: '1896d3ba-715a-47a0-a4e9-c746b69589a2', name: 'Foo' },
-		{ id: '8fdac97d-4a7f-452f-9e74-77bd652ed731', name: 'Bar' },
-		{ id: '7e5da25b-bcde-41bc-91f8-932606080675', name: 'Foo Bar' },
+		{
+			id: '1896d3ba-715a-47a0-a4e9-c746b69589a2',
+			name: 'Foo',
+			phone: '111122223333',
+			address: '4276 Katanga Dr N, Jacksonville, FL 32209',
+		},
+		{
+			id: '8fdac97d-4a7f-452f-9e74-77bd652ed731',
+			name: 'Bar',
+			phone: '444455556666',
+			address: '821 N Jefferson St, Jacksonville, FL 32202',
+		},
+		{
+			id: '7e5da25b-bcde-41bc-91f8-932606080675',
+			name: 'Foo Bar',
+			phone: '777788889999',
+			address: '308 N Julia St, Jacksonville, FL 32202',
+		},
 	])
 
 	const [IsUserModalOpen, SetIsUserModalOpen] = useState(false)
@@ -30,32 +45,58 @@ const Users = () => {
 	const [PhoneValue, SetPhoneValue] = useState('')
 	const [AddressValue, SetAddressValue] = useState('')
 
+	const [SelectedUserId, SetSelectedUserId] = useState()
+
 	const ShowUserModal = () => {
 		SetIsUserModalOpen(true)
 	}
 
 	const HideUserModal = () => {
+		SetNameValue('')
+		SetPhoneValue('')
+		SetAddressValue('')
+
+		SetSelectedUserId(null)
+
 		SetIsUserModalOpen(false)
 	}
 
 	const AddUser = event => {
 		event.preventDefault()
 
-		SetUsersList(users => [
-			...users,
-			{
-				id: uuidv4(),
-				name: NameValue,
-				phone: PhoneValue,
-				address: AddressValue,
-			},
-		])
-
-		SetNameValue('')
-		SetPhoneValue('')
-		SetAddressValue('')
+		if (SelectedUserId) {
+			SetUsersList(users => ({
+				...users,
+				[SelectedUserId]: {
+					name: NameValue,
+					phone: PhoneValue,
+					address: AddressValue,
+				},
+			}))
+		} else {
+			SetUsersList(users => ({
+				...users,
+				[uuidv4()]: {
+					name: NameValue,
+					phone: PhoneValue,
+					address: AddressValue,
+				},
+			}))
+		}
 
 		HideUserModal()
+	}
+
+	const SelectUser = id => {
+		SetSelectedUserId(id)
+
+		const user = UsersList[id]
+
+		SetNameValue(user.name)
+		SetPhoneValue(user.phone)
+		SetAddressValue(user.address)
+
+		ShowUserModal()
 	}
 
 	return (
@@ -64,13 +105,13 @@ const Users = () => {
 				<AddButton onClick={ShowUserModal}>Add</AddButton>
 			</Header>
 			<ListContainer>
-				{UsersList.map(user => (
-					<ListItem key={user.id}>
-						<p>{user.name}</p>
+				{Object.keys(UsersList).map(id => (
+					<ListItem key={id} onClick={() => SelectUser(id)}>
+						<p>{UsersList[id].name}</p>
 					</ListItem>
 				))}
 			</ListContainer>
-			<ReactPortal wrapperId='remove-clock-timezone'>
+			<ReactPortal wrapperId='user-modal'>
 				{IsUserModalOpen && (
 					<ModalContainer>
 						<ModalContentContainer onSubmit={AddUser}>
