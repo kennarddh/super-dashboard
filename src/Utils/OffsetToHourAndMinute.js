bacb14839bd5dda9760e3f9917fb55b69b9e5bf6
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-04 20:51:16 +0700

    feat: Offset to hour and minute util absolute

--- src/Utils/OffsetToHourAndMinute.js
+++ src/Utils/OffsetToHourAndMinute.js
@@ -1,8 +1,12 @@
 const OffsetToHourAndMinute = offset => {
+	const sign = Math.sign(offset)
+
+	offset = Math.abs(offset)
+
 	const hour = Math.floor(offset / 3600)
 	const minute = Math.floor((offset % 3600) / 60)
 
-	return { hour, minute }
+	return { hour, minute, sign }
 }
 
 export default OffsetToHourAndMinute
