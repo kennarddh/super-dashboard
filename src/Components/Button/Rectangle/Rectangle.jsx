328bd167e74f8f2a48d8da837fbe21e9b47368d9
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-07 16:35:31 +0700

    feat: Check is radius number

--- src/Components/Button/Rectangle/Rectangle.jsx
+++ src/Components/Button/Rectangle/Rectangle.jsx
@@ -3,7 +3,9 @@ import styled from 'styled-components'
 import BaseButton from 'Components/Button/Base/Base'
 
 const RectangleButton = styled(BaseButton)`
-	--radius: ${({ radius }) => radius ?? 0}px;
+	--radius: ${({ radius }) =>
+		typeof radius === 'number' ? `${radius ?? 0}px` : radius};
+
 	--height: ${({ height }) => height}px;
 	--width: ${({ width }) => width}px;
 
--- src/Components/Button/Round/Round.jsx
+++ src/Components/Button/Round/Round.jsx
@@ -3,7 +3,9 @@ import styled from 'styled-components'
 import BaseButton from 'Components/Button/Base/Base'
 
 const RoundButton = styled(BaseButton)`
-	--radius: ${({ radius }) => radius ?? 20}px;
+	--radius: ${({ radius }) =>
+		typeof radius === 'number' ? `${radius ?? 20}px` : radius};
+
 	--height: calc(var(--radius) * 2);
 	--width: var(--height);
 
