6e9749198cad1b3894c268897363b0c9d78a3136
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-08 18:39:21 +0700

    feat: Width and height props, default padding and border radius

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -13,27 +13,50 @@ export const Modal = styled.div`
 `
 
 export const ModalContent = styled.div`
-	width: var(--width);
-	height: var(--height);
+	width: ${({ width }) => {
+		if (width) {
+			return typeof padding === 'number' ? `${width}px` : width
+		}
+
+		return '80%'
+	}};
+
+	height: ${({ height }) => {
+		if (height) {
+			return typeof padding === 'number' ? `${height}px` : height
+		}
+
+		return '50%'
+	}};
 
 	background-color: ${({ backgroundColor }) => backgroundColor ?? '#ffffff'};
 
 	border: ${({ border }) => border ?? 'none'};
-	color: ${({ color }) => color ?? '#ffffff'};
+	color: ${({ color }) => color ?? '#000000'};
 
-	padding: ${({ padding }) =>
-		typeof padding === 'number' ? `${padding}px` : padding};
+	padding: ${({ padding }) => {
+		if (padding) {
+			return typeof padding === 'number' ? `${padding}px` : padding
+		}
+
+		return '15px'
+	}};
 
 	margin: ${({ margin }) =>
 		typeof margin === 'number' ? `${margin}px` : margin};
 
-	border-radius: ${({ radius }) =>
-		typeof radius === 'number' ? `${radius}px` : radius};
+	border-radius: ${({ radius }) => {
+		if (radius) {
+			return typeof radius === 'number' ? `${radius}px` : radius
+		}
+
+		return '15px'
+	}};
 
 	display: flex;
 	justify-content: space-evenly;
 	align-items: center;
 
-	flex-direction: column;
+	flex-direction: ${({ flexDirection }) => flexDirection ?? 'column'};
 	gap: 20px;
 `
