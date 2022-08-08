b9ea49b990019f73c5514c70cadcd8160e8c5263
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-08 22:39:29 +0700

    feat: Modal component

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,3 +1,32 @@
+import React, { forwardRef, useImperativeHandle, useState } from 'react'
+
+import ReactPortal from 'Components/ReactPortal/ReactPortal'
+
 import { ModalContainer, ModalContent } from './Styles'
+
+const Modal = ({ wrapperId, containerProps, contentProps, children }, ref) => {
+	const [IsOpen, SetIsOpen] = useState(false)
+
+	useImperativeHandle(ref, () => ({
+		Open() {
+			SetIsOpen(true)
+		},
+		Close() {
+			SetIsOpen(false)
+		},
+	}))
+
+	return (
+		<ReactPortal wrapperId={wrapperId}>
+			{IsOpen && (
+				<ModalContainer {...containerProps}>
+					<ModalContent {...contentProps}>{children}</ModalContent>
+				</ModalContainer>
+			)}
+		</ReactPortal>
+	)
+}
+
+export default forwardRef(Modal)
 
 export { ModalContainer, ModalContent }
