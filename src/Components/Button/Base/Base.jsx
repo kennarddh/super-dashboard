31f750f7295f076b425f74150f5df9999623896f
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-09 14:57:10 +0700

    fix: Base button default props

--- src/Components/Button/Base/Base.jsx
+++ src/Components/Button/Base/Base.jsx
@@ -4,10 +4,10 @@ const BaseButton = styled.button`
 	width: var(--width);
 	height: var(--height);
 
-	background-color: ${({ backgroundColor }) => backgroundColor ?? '#a0a0a0'};
+	background-color: ${({ backgroundColor }) => backgroundColor};
 
-	border: ${({ border }) => border ?? 'none'};
-	color: ${({ color }) => color ?? '#ffffff'};
+	border: ${({ border }) => border};
+	color: ${({ color }) => color};
 
 	padding: ${({ padding }) =>
 		typeof padding === 'number' ? `${padding}px` : padding};
@@ -35,24 +35,34 @@ const BaseButton = styled.button`
 
 			${top &&
 			css`
-				top: ${top}px;
+				top: ${({ top }) =>
+					typeof top === 'number' ? `${top}px` : top};
 			`}
 
 			${bottom &&
 			css`
-				bottom: ${bottom}px;
+				bottom: ${({ bottom }) =>
+					typeof bottom === 'number' ? `${bottom}px` : bottom};
 			`}
 
 			${right &&
 			css`
-				right: ${right}px;
+				right: ${({ right }) =>
+					typeof right === 'number' ? `${right}px` : right};
 			`}
 
 			${left &&
 			css`
-				top: ${left}px;
+				left: ${({ left }) =>
+					typeof left === 'number' ? `${left}px` : left};
 			`}
 		`}
 `
+
+BaseButton.defaultProps = {
+	backgroundColor: '#a0a0a0',
+	border: 'none',
+	color: '#ffffff',
+}
 
 export default BaseButton
