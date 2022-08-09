fd1cfba8e3313f0ad093334978a51f4c556a69e4
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-09 14:16:51 +0700

    refactor: Remove unused export users modal styles

--- src/Components/Users/Modal/Styles.jsx
+++ src/Components/Users/Modal/Styles.jsx
@@ -16,26 +16,3 @@ export const Buttons = styled.div`
 	gap: 20px;
 	flex-direction: row;
 `
-
-export const ModalContainer = styled.div`
-	position: absolute;
-	inset: 0;
-	background-color: #b4b4b4a4;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-`
-
-export const ModalContentContainer = styled.form`
-	height: 80%;
-	width: 50%;
-	background-color: #ffffff;
-	border-radius: 15px;
-	padding: 20px;
-	box-sizing: border-box;
-	display: flex;
-	flex-direction: column;
-	gap: 20px;
-	justify-content: space-evenly;
-	align-items: center;
-`
