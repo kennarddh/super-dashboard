32fbd99ae51e6756a97db32fdc952d8803c86ded
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-17 01:26:01 +0700

    feat: Change get document vertical center util

--- src/Utils/GetDocumentVerticalCenter.js
+++ src/Utils/GetDocumentVerticalCenter.js
@@ -1,10 +1,5 @@
 const GetDocumentVerticalCenter = () => {
-	return (
-		(window.scrollY < document.body.clientHeight / 2
-			? window.scrollY
-			: window.scrollY * -1) +
-		window.innerHeight / 2
-	)
+	return window.scrollY + document.body.clientHeight / 2
 }
 
 export default GetDocumentVerticalCenter
