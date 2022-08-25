857fbaee56e01561a02883be6edaee6548b1e739
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-25 19:11:54 +0700

    refactor: Fix typo

--- src/Utils/ParseMathString/ParseMathExpression.test.js
+++ src/Utils/ParseMathString/ParseMathExpression.test.js
@@ -2,30 +2,31 @@ import ParseMathExpression from './ParseMathExpression'
 
 describe('ParseMathExpression', () => {
 	it.each([
-		{ expresion: '10+20', expected: 30 },
-		{ expresion: '(10+10*10)^2', expected: 12100 },
-		{ expresion: '20+ 10 - 80 * 10', expected: -770 },
-		{ expresion: '20*10+10 /20 ', expected: 200.5 },
-		{ expresion: '(10 + 10) * 10', expected: 200 },
-		{ expresion: '10 - 20 + 10', expected: 0 },
-		{ expresion: '30 + (10 * 2)^3', expected: 8030 },
-		{ expresion: '0.5 + 0.5', expected: 1 },
-		{ expresion: '0.5 * 3 + 10', expected: 11.5 },
+		{ expression: '10+20', expected: 30 },
+		{ expression: '(10+10*10)^2', expected: 12100 },
+		{ expression: '20+ 10 - 80 * 10', expected: -770 },
+		{ expression: '20*10+10 /20 ', expected: 200.5 },
+		{ expression: '(10 + 10) * 10', expected: 200 },
+		{ expression: '10 - 20 + 10', expected: 0 },
+		{ expression: '30 + (10 * 2)^3', expected: 8030 },
+		{ expression: '0.5 + 0.5', expected: 1 },
+		{ expression: '0.5 * 3 + 10', expected: 11.5 },
 		{
-			expresion: '30^2 * 10 / 20 +10 *20*(10- 20 *3)  + (10^4 + 10) + 10',
+			expression:
+				'30^2 * 10 / 20 +10 *20*(10- 20 *3)  + (10^4 + 10) + 10',
 			expected: 470,
 		},
-		{ expresion: '3^2^2', expected: 81 },
+		{ expression: '3^2^2', expected: 81 },
 		{
-			expresion: '10 * (10 + (10 / 100 )) + (10 * (10 - 20))',
+			expression: '10 * (10 + (10 / 100 )) + (10 * (10 - 20))',
 			expected: 1,
 		},
 	])(
-		'Should success, Expresion: $expresion, expected: $expected',
-		({ expresion, expected }) => {
+		'Should success, Expression: $expression, expected: $expected',
+		({ expression, expected }) => {
 			expect.assertions(1)
 
-			expect(ParseMathExpression(expresion)).toBe(expected)
+			expect(ParseMathExpression(expression)).toBe(expected)
 		}
 	)
 })
