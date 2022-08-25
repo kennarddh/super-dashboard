be8d36065a90d97e50c814ed39976d25d9337190
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-25 19:25:07 +0700

    fix: Parse number error with minus

--- src/Utils/ParseMathString/ParseNumber.js
+++ src/Utils/ParseMathString/ParseNumber.js
@@ -2,7 +2,7 @@ const ParseNumber = noStr => {
 	const isMinus = noStr.trim().slice(0, 5) === 'minus'
 
 	return isMinus
-		? parseFloat(noStr.trim().slice(6, noStr.length - 1), 10) * -1
+		? parseFloat(noStr.trim().slice(6, noStr.length), 10) * -1
 		: parseFloat(noStr, 10)
 }
 
