e17b30412fd7fe1e2e40a4dbf53ac7ab942d3f40
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-27 18:59:53 +0700

    feat: Support pi and e replace minus

--- src/Utils/ParseMathExpression/ReplaceMinus.js
+++ src/Utils/ParseMathExpression/ReplaceMinus.js
@@ -1,7 +1,14 @@
+import Symbols from 'Constants/Math/Symbols'
+
 const ReplaceMinus = expression =>
-	expression.replaceAll(
-		/\(-\d+\)/g,
-		numberString => `minus_${numberString.slice(2).slice(0, -1)}`
+	expression.replace(
+		new RegExp(
+			`\\((-\\d+)|${Object.keys(Symbols)
+				.map(str => `(\\(-${str}\\))`)
+				.join('|')}\\)`,
+			'g'
+		),
+		numberString => `minus_${numberString.slice(2, -1)}`
 	)
 
 export default ReplaceMinus
