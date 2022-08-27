8fb9327c1ab1f3e0ce1faad4c5411e6ca5900ee8
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-27 13:59:24 +0700

    refactor: Use replace with global regex instead of replace all

--- src/Utils/ParseMathExpression/Parser/MathSymbols.js
+++ src/Utils/ParseMathExpression/Parser/MathSymbols.js
@@ -4,7 +4,7 @@ const MathSymbols = expression =>
 	Object.entries(Symbols)
 		.reduce(
 			(acc, [alias, { value }]) =>
-				acc.replaceAll(
+				acc.replace(
 					new RegExp(
 						`([^a-zA-Z0-9]${alias}[^a-zA-Z0-9]|${alias} | ${alias} | ${alias}|(^${alias}$))`,
 						'g'
