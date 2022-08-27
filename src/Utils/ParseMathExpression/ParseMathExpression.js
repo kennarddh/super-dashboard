4d7a2c4ead770d4a425599ca97e6f1468dcbeda3
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-27 14:04:28 +0700

    feat: Parse square root

--- src/Utils/ParseMathExpression/ParseMathExpression.js
+++ src/Utils/ParseMathExpression/ParseMathExpression.js
@@ -3,6 +3,7 @@ import ReplaceMinus from 'Utils/ParseMathExpression/ReplaceMinus'
 import FormatExpression from 'Utils/ParseMathExpression/FormatExpression'
 import ParseMathSymbols from 'Utils/ParseMathExpression/Parser/MathSymbols'
 import ParseFactorial from 'Utils/ParseMathExpression/Parser/Factorial'
+import ParseSquareRoot from 'Utils/ParseMathExpression/Parser/SquareRoot'
 
 const ParseMathExpression = expression => {
 	const operators = ['-', '+', '*', '/', '^', '%']
@@ -14,8 +15,9 @@ const ParseMathExpression = expression => {
 	const symbolsParsed = ParseMathSymbols(formatted)
 
 	const factorialParsed = ParseFactorial(symbolsParsed, operators)
+	const squareRootParsed = ParseSquareRoot(factorialParsed, operators)
 
-	return ParsePlus(factorialParsed)
+	return ParsePlus(squareRootParsed)
 }
 
 export default ParseMathExpression
