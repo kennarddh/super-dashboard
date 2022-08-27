948825357254fb6956fc197e7c710eed8d5c26a6
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-27 19:35:14 +0700

    fix: Replace minus

--- src/Utils/ParseMathExpression/ReplaceMinus.js
+++ src/Utils/ParseMathExpression/ReplaceMinus.js
@@ -3,9 +3,9 @@ import Symbols from 'Constants/Math/Symbols'
 const ReplaceMinus = expression =>
 	expression.replace(
 		new RegExp(
-			`\\((-\\d+)|${Object.keys(Symbols)
-				.map(str => `(\\(-${str}\\))`)
-				.join('|')}\\)`,
+			`(\\(-\\d+\\)|${Object.keys(Symbols)
+				.map(str => `\\(-${str}\\)`)
+				.join('|')})`,
 			'g'
 		),
 		numberString => `minus_${numberString.slice(2, -1)}`
