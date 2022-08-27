b3753f914ea89852379603af468c4e81a2f44db2
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-27 14:00:24 +0700

    fix: Use same regex for preview math symbols

--- src/Utils/ParseMathExpression/PreviewMathSymbols.js
+++ src/Utils/ParseMathExpression/PreviewMathSymbols.js
@@ -1,9 +1,18 @@
 import Symbols from 'Constants/Math/Symbols'
 
-const PreviewMathSymbols = expression =>
-	Object.entries(Symbols).reduce(
-		(acc, [alias, { symbol }]) => acc.replaceAll(alias, symbol),
-		expression
-	)
+const MathSymbols = expression =>
+	Object.entries(Symbols)
+		.reduce(
+			(acc, [alias, { symbol }]) =>
+				acc.replace(
+					new RegExp(
+						`([^a-zA-Z0-9]${alias}[^a-zA-Z0-9]|${alias} | ${alias} | ${alias}|(^${alias}$))`,
+						'g'
+					),
+					` ${symbol} `
+				),
+			expression
+		)
+		.trim()
 
-export default PreviewMathSymbols
+export default MathSymbols
