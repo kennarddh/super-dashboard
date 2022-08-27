1adf5d52f4b6672cb22d40a31557b25a10d16c69
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-27 13:57:35 +0700

    fix: Math symbol replace using regex

--- src/Utils/ParseMathExpression/Parser/MathSymbols.js
+++ src/Utils/ParseMathExpression/Parser/MathSymbols.js
@@ -1,9 +1,18 @@
 import Symbols from 'Constants/Math/Symbols'
 
 const MathSymbols = expression =>
-	Object.entries(Symbols).reduce(
-		(acc, [alias, { value }]) => acc.replaceAll(alias, value().toString()),
-		expression
-	)
+	Object.entries(Symbols)
+		.reduce(
+			(acc, [alias, { value }]) =>
+				acc.replaceAll(
+					new RegExp(
+						`([^a-zA-Z0-9]${alias}[^a-zA-Z0-9]|${alias} | ${alias} | ${alias}|(^${alias}$))`,
+						'g'
+					),
+					` ${value()} `
+				),
+			expression
+		)
+		.trim()
 
 export default MathSymbols
