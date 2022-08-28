31d91caddf6a7a0141b2039fb7b760d76b0d5b4e
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-08-28 14:22:25 +0700

    perf: Change factorial from recursive to iterative

--- src/Utils/Math/Factorial/Factorial.js
+++ src/Utils/Math/Factorial/Factorial.js
@@ -2,7 +2,13 @@ const Factorial = num => {
 	if (num < 0) return -1
 	if (num == 0) return 1
 
-	return num * Factorial(num - 1)
+	let result = 1
+
+	for (let i = 1; i <= num; i += 1) {
+		result = result * i
+	}
+
+	return result
 }
 
 export default Factorial
