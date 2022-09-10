06d038760b42df306ba8973128489d7c0c8d4097
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-09-10 15:00:36 +0700

    feat: Tool bar button theme

--- src/Components/ToolBar/ToolBar.jsx
+++ src/Components/ToolBar/ToolBar.jsx
@@ -33,4 +33,7 @@ export const ToolbarButton = styled.button`
 	background-color: #fafafa;
 	box-shadow: 0 13px 27px -5px rgba(50, 50, 93, 0.25),
 		0 8px 16px -8px rgba(0, 0, 0, 0.3);
+
+	background-color: ${({ theme }) => theme.toolBar.backgroundColor};
+	color: ${({ theme }) => theme.toolBar.color};
 `
